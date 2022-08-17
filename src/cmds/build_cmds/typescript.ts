ffba732351dd19bd6fea798123642bee30587ac8
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-08-17 08:46:48 +0200

    v1.0.0

---
+++ src/cmds/build_cmds/typescript.ts
@@ -0,0 +1,24 @@
+import * as path from 'path';
+
+import { Build } from '../../classes/build/build';
+import { config } from '../../config/config';
+
+exports.command = 'typescript [source] [dist] [typesOnly]';
+exports.alias = 'ts';
+exports.desc = 'Generate typescript models';
+exports.builder = {
+	source: {
+		default: `${config.host}:${config.port.spec}`,
+	},
+	dist: {
+		default: config.typescript.dist,
+	},
+	typesOnly: {
+		default: config.typescript.typesOnly,
+	},
+};
+exports.handler = (argv: any) => {
+	const source = /^https?:\/\//.test(argv.source) ? argv.source : path.resolve(path.join(process.cwd(), argv.source));
+	const dist = path.resolve(path.join(process.cwd(), argv.dist));
+	Build.typescript(source, dist, argv.typesOnly !== 'false');
+};
---
+++ src/config/config.ts
@@ -0,0 +1,17 @@
+export const config = {
+	basePath: '/v1',
+	host: 'http://localhost',
+	port: {
+		editor: '3010',
+		spec: '3011',
+	},
+	dist: {
+		folder: './dist',
+		fileName: 'index',
+	},
+	sourceUrl: './src/docs/index.json',
+	typescript: {
+		dist: './src/rest/api',
+		typesOnly: true,
+	},
+};
